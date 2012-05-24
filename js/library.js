c0af018ded75c531532d3f739fe6dd6658568ee7
Illastr8 <Illastr8@Illastr8s-MacBook-Pro.local>
2012-05-24 12:52:30 -0500

    Adding Phone String to library & console log to js file.

--- js/library.js
+++ js/library.js
@@ -1,1 +1,20 @@
+/* 
+Author: Marco Rodriguez
+Date: 05-24-12
+SDI - Project 4
+Buliding my library...
+*/
+
 // library
+
+// 1 - string that checks if a string is a phone number checks if the number follows the phone number pattern. Does a string follow a 123-456-7890 pattern like a phone number?
+
+
+var checkPhone = function(validate) {
+var checkNumbers = /^([0-9]{3})\-([0-9]{3})\-([0-9]{4})$/;
+	if(checkNumbers.test(validate)){
+		return true;
+	} else {
+		return false;
+	}
+};
--- js/rodriguez_marco_assignment4.js
+++ js/rodriguez_marco_assignment4.js
@@ -1,1 +1,17 @@
-alert("JavaScript works!");
+/* 
+Author: Marco Rodriguez
+Date: 05-24-12
+SDI - Project 4
+Buliding my library...
+*/
+
+// testing
+
+// 1 - string that checks if a string follows pattern
+
+console.log(checkPhone("214-334-4578")); // follows number pattern
+console.log(checkPhone("972-228-789S")); // false 
+console.log(checkPhone("972-228-7899")); // follows number pattern
+
+
+console.log(" ");
